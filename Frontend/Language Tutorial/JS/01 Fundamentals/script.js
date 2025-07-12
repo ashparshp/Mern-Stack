417c51198c7f5780b52b11fcfb84c70ba6c965c9
Ashparshp <ashparsh.connects@gmail.com>
2025-07-12 17:09:47 +0530

    feat: type conversion and coercion examples in script

--- Frontend/Language Tutorial/JS/01 Fundamentals/script.js
+++ Frontend/Language Tutorial/JS/01 Fundamentals/script.js
@@ -35,4 +35,14 @@ if (hasDriversLicense && hasGoodVision) {
 
 // type conversion
 const inputYear = "1991";
-console.log(Number(inputYear) + 18); // Converts string to number
+console.log(Number(inputYear) + 18); // Converts string to number but does not change the original string
+console.log(String(23)); // Converts number to string
+console.log(Number("Ashparsh")); // NaN (Not a Number) because "Ashparsh" cannot be converted to a number
+console.log(typeof NaN); // NaN is of type 'number'
+
+// type coercion
+console.log("I am " + 23 + " years old."); // Coerces number to string
+console.log("23" - "10"); // Coerces strings to numbers and performs subtraction
+console.log("23" + "10"); // Concatenates strings
+console.log("23" * "2"); // Coerces strings to numbers and performs multiplication
+console.log("23" / "2"); // Coerces strings to numbers and performs division
