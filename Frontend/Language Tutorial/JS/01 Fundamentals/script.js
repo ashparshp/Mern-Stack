327bf9314a8ad8c02e39921c5268b361b04c6f90
Ashparshp <ashparsh.connects@gmail.com>
2025-07-12 15:59:17 +0530

    feat: Add constant and comments on variable declarations

--- Frontend/Language Tutorial/JS/01 Fundamentals/script.js
+++ Frontend/Language Tutorial/JS/01 Fundamentals/script.js
@@ -2,3 +2,8 @@ let JS = "Amazing";
 if (JS === "Amazing") alert("JS is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+
+const age = 23;
+// age = 24; // This will throw an error because 'age' is a constant
+
+// const birthYear; // This will throw an error because 'birthYear' is not initialized
